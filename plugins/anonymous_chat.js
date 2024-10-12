f26e086a937b5c97a9da0333dd1c8bfe285b18d2
dhiyzl <86110711+itsdzl@users.noreply.github.com>
2024-10-12 11:11:17 +0700

    Update anonymous_chat.js

--- plugins/anonymous_chat.js
+++ plugins/anonymous_chat.js
@@ -1,4 +1,4 @@
-const { MessageType } = require("@adiwajshing/baileys")
+const { MessageType } = require("@bot-wa/bot-wa-baileys")
 
 async function handler(m, { command }) {
     command = command.toLowerCase()
