92e735946191583e42ce1cf70f63a2a88e057890
dhiyzl <86110711+itsdzl@users.noreply.github.com>
2024-10-12 11:49:08 +0700

    Update fitnah.js

--- plugins/fitnah.js
+++ plugins/fitnah.js
@@ -1,4 +1,4 @@
-let { MessageType } = require('@adiwajshing/baileys')
+let { MessageType } = require('@bot-wa/bot-wa-baileys')
 let handler = async (m, { conn, text }) => {
   if (!text) return
   let cm = copy(m)
