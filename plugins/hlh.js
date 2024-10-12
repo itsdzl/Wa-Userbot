74ce387884c2ed7cf5cb369e39eed6bceb6caa8f
DzLyz <86110711+itsdzl@users.noreply.github.com>
2024-10-12 11:03:53 +0700

    Update hlh.js

--- plugins/hlh.js
+++ plugins/hlh.js
@@ -1,4 +1,4 @@
-let { MessageType } = require('@adiwajshing/baileys')
+let { MessageType } = require('@bot-wa/bot-wa-baileys')
 let handler = async (m, { command, text, DevMode }) => {
     try {
         let ter = command[1].toLowerCase()
