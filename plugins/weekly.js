9496dc3305b718729475b0af834d3b8230eeb2ae
dhiyzl <86110711+itsdzl@users.noreply.github.com>
2024-10-12 11:13:49 +0700

    Update weekly.js

--- plugins/weekly.js
+++ plugins/weekly.js
@@ -1,4 +1,4 @@
-let { MessageType } = require('@adiwajshing/baileys')
+let { MessageType } = require('@bot-wa/bot-wa-baileys')
 
 let handler = async (m, { conn }) => {
     let user = global.DATABASE._data.users[m.sender]
