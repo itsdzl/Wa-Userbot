2891812068d6de1cd78217b6112fd5eaf9043f0e
dhiyzl <86110711+itsdzl@users.noreply.github.com>
2024-10-12 11:50:56 +0700

    Update sgura.js

--- plugins/sgura.js
+++ plugins/sgura.js
@@ -1,4 +1,4 @@
-const { MessageType } = require('@adiwajshing/baileys')
+const { MessageType } = require('@bot-wa/bot-wa-baileys')
 const { sticker } = require('../lib/sticker')
 let handler = async (m, { conn, text }) => {
   if (text.length > 7) throw 'Max. 7 huruf!'
